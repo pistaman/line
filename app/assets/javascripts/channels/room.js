0db28dcb48a4ddff836cebeadf6c70a5830c22ae
nishi <pisces.nisikawa@gmail.com>
2018-08-07 11:45:10 +0900

    DB保存できるようにした

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,25 +1,44 @@
-App.room = App.cable.subscriptions.create({
-  channel: "RoomChannel",
-  room_id: $('#messages').data('room_id'),
-  connected: function () {
-    // Called when the subscription is ready for use on the server
-  },
+// App.room = App.cable.subscriptions.create({
+//   channel: "RoomChannel",
+//   room_id: $('#messages').data('room_id'),
+//   connected: function () {
+//     // Called when the subscription is ready for use on the server
+//   },
 
-  disconnected: function () {
-    // Called when the subscription has been terminated by the server
-  },
+//   disconnected: function () {
+//     // Called when the subscription has been terminated by the server
+//   },
 
-  received: function (data) {
-    $('#messages').append(data['message']);
-  },
+//   received: function (data) {
+//     $('#messages').append(data['message']);
+//   },
 
-  speak: function (message) {
-    // return this.perform('speak');
-    return this.perform('speak', {
-      message: message
+//   speak: function (message) {
+//     // return this.perform('speak');
+//     return this.perform('speak', {
+//       message: message
+//     });
+//   }
+// });
+
+document.addEventListener('turbolinks:load', function () {
+  return App.room = App.cable.subscriptions.create({
+    channel: "RoomChannel",
+    room_id: $('#messages').data('room_id')
+  }, {
+      connected: function () { },
+      disconnected: function () { },
+      received: function (data) {
+        return $('#messages').append(data['message']);
+      },
+      speak: function (message) {
+        return this.perform('speak', {
+          message: message
+        });
+      }
     });
-  }
 });
+
 
 $(document).on('keypress', '[data-behavior~=room_speaker]', function (event) {
   if (event.keyCode === 13) {
