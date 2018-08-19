30eed02aa9b195714de4f3a94dc7f8eeab66902f
nishi <pisces.nisikawa@gmail.com>
2018-08-19 13:17:22 +0900

    fix

--- db/migrate/20180807014309_rename_foreign_column_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class RenameForeignColumnToMessages < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :messages, :users_id, :user_id
-    rename_column :messages, :rooms_id, :room_id
-  end
-end
