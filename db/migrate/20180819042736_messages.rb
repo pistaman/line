4e71b6a426f0e04b82463f1f54b02b43cb6399c0
nishi <pisces.nisikawa@gmail.com>
2018-08-19 14:07:18 +0900

    douyo

--- db/migrate/20180819042736_messages.rb
+++ db/migrate/20180819042736_messages.rb
@@ -1,6 +1,6 @@
 class Messages < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :messages, :seen,false,0
+    change_column_null :messages, :seen,false,false
     change_column_null :messages, :content,false,nil
     change_column_null :messages, :user_id,false,nil
     change_column_null :messages, :room_id,false,nil
