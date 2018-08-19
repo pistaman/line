8692c4d088a6dca73cb990fab61d911fe4bfa60c
nishi <pisces.nisikawa@gmail.com>
2018-08-19 13:35:14 +0900

    commit

--- db/migrate/20180803064022_create_messages.rb
+++ db/migrate/20180803064022_create_messages.rb
@@ -2,8 +2,8 @@ class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
       t.text :content
-      t.references :users, foreign_key: true
-      t.references :rooms, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
       t.boolean :seen
 
       t.timestamps
