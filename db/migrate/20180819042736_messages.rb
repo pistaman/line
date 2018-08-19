4267d14d8ef78017ef47b21f0d954883dfdc6a06
nishi <pisces.nisikawa@gmail.com>
2018-08-19 13:29:35 +0900

    fix migration

--- db/migrate/20180819042736_messages.rb
+++ db/migrate/20180819042736_messages.rb
@@ -1,9 +1,8 @@
-class AddOptionsToMessages < ActiveRecord::Migration[5.2]
+class Messages < ActiveRecord::Migration[5.2]
   def change
     change_column_null :messages, :seen,false,0
     change_column_null :messages, :content,false,nil
     change_column_null :messages, :user_id,false,nil
     change_column_null :messages, :room_id,false,nil
   end
-
 end
